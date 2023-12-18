9f86bf648b02610be2f15d2cea442e221491447b
Coders Club <yavyav@codersclub.com>
2023-12-18 16:37:42 +0300

    slice app first and last

---
+++ src/store/slices/courseSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const courseSlice = createSlice({
+    name: 'form',
+    initialState: {
+        searchTerm: '',
+        data: []
+    },
+    reducers: {
+        addCourse(state, action) {
+            state.data.push({
+                name: action.payload.name,
+                description: action.payload.description,
+                cost: action.payload.cost,
+                id: nanoid(),
+            })
+        },
+        changeSearchTerm(state,action){
+            state.searchTerm = action.payload;
+        },
+        removeCourse(state, action){
+            const updatedCourses = state.data.filter((course) => {
+                return course.id !== action.payload;
+            });
+            state.data = updatedCourses;
+        }
+    },
+});
+
+export const { addCourse, removeCourse, changeSearchTerm } = courseSlice.actions;
+export const courseReducer = courseSlice.reducer;
